9201c8b67d1ee8010f2dbc741c2a5427f138f092
Nitinyadav14920 <77772979+Nitinyadav14920@users.noreply.github.com>
2023-12-24 21:22:20 +0530

    Context API

---
+++ 08-project/mini-Context/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>React with mini Context</h1>
+      <Login />
+      <Profile />
+    </ UserContextProvider>
+  )
+}
+
+export default App
